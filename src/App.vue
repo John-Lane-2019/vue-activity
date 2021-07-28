aec62709b190013d21cda729210df52037ce31f6
John <jlane7590@yahoo.ca>
2021-07-28 15:25:03 -0400

    82: Moving form to separate component

--- src/App.vue
+++ src/App.vue
@@ -22,47 +22,7 @@
     <section class="container">
       <div class="columns">
         <div class="column is-3">
-          <a v-if="!isFormDisplayed" @click="toggleFormDisplay" class="button is-primary is-block is-alt is-large" href="#">New Activity</a>
-          <div v-if="isFormDisplayed" class="create-form">
-            <h2>Create Activity</h2>
-            <form>
-              <div class="field">
-                <label class="label">Title</label>
-                <div class="control">
-                  <input v-model="newActivity.title" class="input" type="text" placeholder="Read a Book">
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Notes</label>
-                <div class="control">
-                  <textarea v-model="newActivity.notes" class="textarea" placeholder="Write some notes here"></textarea>
-                </div>
-              </div>
-              <div class="field">
-                <label class="label">Notes</label>
-                <div class="control">
-                  <select v-model="newActivity.category" class="select">
-                    <option disabled value="">Please Select One</option>
-                    <option 
-                      v-for="category in categories" 
-                      :key="category.id">{{ category.text }}
-                    </option>
-                  </select>
-                </div>
-              </div>
-              <div class="field is-grouped">
-                <div class="control">
-                  <button 
-                    @click="createActivity" 
-                    class="button is-link"
-                    :disabled="!isFormValid">Create Activity</button>
-                </div>
-                <div class="control">
-                  <button class="button is-text" @click="toggleFormDisplay">Cancel</button>
-                </div>
-              </div>
-            </form>
-          </div>
+        <ActivityCreate :categories="categories"/>
         </div>
         <div class="column is-9">
           <div class="box content">
@@ -81,21 +41,16 @@
 </template>  
 <script>
 import ActivityItem from '@/components/ActivityItem'
+import ActivityCreate from '@/components/ActivityCreate'
 import { fetchActivities, fetchUser, fetchCategories } from '@/api'
 export default {
   name: 'app',
-  components: {ActivityItem},
+  components: {ActivityItem, ActivityCreate},
   data () {
     return {
-      isFormDisplayed: false,
       creator: 'John Lane',
       appName: 'Activity Planner',
       watchedAppName: 'Activity Planner by John Lane',
-      newActivity: {
-        title: '',
-        notes: '',
-        category: '',
-      },
       items: {1: {name: 'Filip'}, 2: {name: 'John'}},
         user: {},
         activities: {},
@@ -103,10 +58,7 @@ export default {
     }
   },
   computed:{
-    isFormValid() {
-      return this.newActivity.title && this.newActivity.notes
-      //using computed property because they are cached. 
-    },
+    
     fullAppName(){ 
       return `${this.appName} by ${this.creator}`
     },
@@ -129,12 +81,7 @@ export default {
     this.categories = fetchCategories()
   },
   methods: {
-    toggleFormDisplay () {
-      this.isFormDisplayed = !this.isFormDisplayed
-    },
-    createActivity () {
-      console.log(this.newActivity)
-    },
+    
   }
 }
 </script>
